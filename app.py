33dd319a59b56ae35e16d0bceb243412901aac47
Atiqur Rahaman <60899605+atiqureee51@users.noreply.github.com>
2025-01-07 03:42:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from utils import *
 from config import NREL_API_KEY, EMAIL
 from geopy.geocoders import Nominatim
 import io
+import os
 
 
 
@@ -1117,4 +1118,5 @@ def get_api_config():
     return jsonify({'success':True,'api_key':NREL_API_KEY,'email':EMAIL})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=True, host='0.0.0.0', port=port)
